cf085e87da10b48f998de7a726885472d356ca47
Chanon <chanongul@gmail.com>
2023-10-29 20:13:14 +0700

    🛠️ Update store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,16 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { persistReducer } from "redux-persist";
+import { persistStore, persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
 import storage from "redux-persist/lib/storage";
-import authReducer from "@/features/auth/auth-slice";
-import wsReducer from "@/features/ws/ws-slice";
+import authReducer from "@/features/auth/slice";
+import lqsReducer from "@/features/ws/store/slices/lqs";
+import modReducer from "@/features/ws/store/slices/mod";
+import lobbyReducer from "@/features/lobby/store/slice";
+import wsMiddleware from "@/features/ws/store/middleware";
+import WS from "@/features/ws/utils/ws";
 
 const persistConfig = {
+  timeout: 500,
   key: "root",
   version: 1,
   storage,
@@ -14,7 +20,9 @@ const persistedReducer = persistReducer(
   persistConfig,
   combineReducers({
     auth: authReducer,
-    ws: wsReducer,
+    lqs: lqsReducer,
+    lobby: lobbyReducer,
+    mod: modReducer,
   })
 );
 
@@ -23,5 +31,7 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }),
+    }).concat(thunk, wsMiddleware(new WS())),
 });
+
+export const persistor = persistStore(store);
